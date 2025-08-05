98fc5faf5fc987454f57034aa6a097cff13b3b88
ebinee <skyim0907@naver.com>
2025-08-05 15:47:02 +0900

    feat(map): implement map api

--- back/src/main/java/com/springboot/be/BeApplication.java
+++ back/src/main/java/com/springboot/be/BeApplication.java
@@ -2,14 +2,10 @@ package com.springboot.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.springboot.be")
 @EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "com.springboot.be.repository")
-@EntityScan(basePackages = "com.springboot.be.entity")
 public class BeApplication {
 
     public static void main(String[] args) {
